4346bf37dd0b1fdb743f19062c5df379b2536565
Jan Juarez <janlolsas272272@gmail.com>
2021-12-04 23:29:21 -0600

    Agregado movimiento del personaje

--- Assets/movimiento.cs
+++ Assets/movimiento.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class movimiento : MonoBehaviour
 {
     // Start is called before the first frame update
- 	public float vel = 0.3f;
+ 	public float vel = 2f;
 
  	public Rigidbody2D rb;
 
  	Vector2 mov;
 
-
+ 	public Animator animator;
     // Update is called once per frame
     void Update()
     {
+    	Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+    	animator.SetFloat("Horizontal", movement.x);
+    	animator.SetFloat("Vertical", movement.y);
+    	animator.SetFloat("Magnitud", movement.magnitude);
         //Input
         mov.x = Input.GetAxisRaw("Horizontal");
         mov.y = Input.GetAxisRaw("Vertical");
