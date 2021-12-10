8c545a334d6096ed69482ee2947a5d4c8be5e698
Edgar Santana <edgarfranciscos@gmail.com>
2021-12-10 11:26:13 -0600

    new/Scene_Inicio

---
+++ Assets/Scripts/paralax2D.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paralax2D : MonoBehaviour
+{
+    [Range(1f, 20F)]
+    public float ScrollSpeed = 1;
+    public float scrollOfset;
+    Vector2 StartPos;
+    float NawPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NawPos = Mathf.Repeat(Time.time * -ScrollSpeed, scrollOfset);
+        transform.position = StartPos + Vector2.right * NawPos;
+    }
+}
